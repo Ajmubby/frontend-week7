bbf21bf57e969068e92c96f8d3fc8d336836d4eb
Ajmubby <ajuwonmubarak4@gmail.com>
2024-01-17 00:18:52 +0100

    feat: add assignment

--- functions.js
+++ functions.js
@@ -1,40 +1,38 @@
-// function sayHello(name, surname){
-//     // let name = 'Mubby'
-//     alert(`Good morning  ${name}, ${surname}`)
-// }
-// sayHello('Ade', 'tola')
+function sayHello(name, surname){
+    // let name = 'Mubby'
+    alert(`Good morning  ${name}, ${surname}`)
+}
+sayHello('Ade', 'tola')
 
-// function addMe(a,b){
-//     return(a + b)  
-// }
+function addMe(a,b){
+    return(a + b)  
+}
+console.log(addMe (4,6))
 
-// console.log(addMe (4,6))
-
-// //addition function
-// function text(){
-//    let a = prompt('give a number')
-//    let b = prompt('give another number')
-//     return (a*1) + (b-0)
-// }
-
-// console.log(text())
+//addition function
+function text(){
+   let a = prompt('give a number')
+   let b = prompt('give another number')
+   //below is to print the result as an int instead of string. You can also use rap it in a number....
+    return (a*1) + (b-0)
+}
+console.log(text())
 
 //getage
-// function age(){
-//     let year = prompt('what year were you born')
-//     let age = new Date().getFullYear() - year
-//     return `You are ${age} years old`
-// }
+function age(){
+    let year = prompt('what year were you born')
+    // dateconstructor
+    let age = new Date().getFullYear() - year
+    return `You are ${age} years old`
+}
+ console.log(age())
 
-//  console.log(age())
-
-// function capitalise(firstName){
-//     firstName= firstName.toLowerCase()
-//     return firstName.replace(firstName.charAt(0), firstName.charAt(0).toUpperCase())
-
-// }
-
-// console.log(capitalise('mubby'))
+//replace the first item and return the first item in uppercase
+function capitalise(firstName){
+    firstName= firstName.toLowerCase()
+    return firstName.replace(firstName.charAt(0), firstName.charAt(0).toUpperCase())
+}
+console.log(capitalise('mubby'))
 
 //Squareroot converted to 3 decimal
 function squaroot(n){
@@ -44,7 +42,7 @@ console.log(squaroot(10))
 
 //square
 function square(n){
-    return (n*n)
+    return (n**2)
 }
 console.log(square(4))
 

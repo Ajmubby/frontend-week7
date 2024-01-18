09437676b8a9ca1a6c5892ea5f7a72dff9634735
Ajmubby <ajuwonmubarak4@gmail.com>
2024-01-18 08:51:09 +0100

    feat : add work

--- arrays.js
+++ arrays.js
@@ -1,25 +1,27 @@
 let students = 'mubarak marvellous kehinde ekene ayodele franca'
 
+
 const arrOfStds = students.split(' ')
 //remove last member from an array
 arrOfStds.pop()
 arrOfStds.pop()
 
 arrOfStds.shift()
-//add to the start of an array
+//insert new elements at the start of an array
 arrOfStds.unshift('Mubarak')
-//add to the end of an array
+//insert new elements at the end of an array
 arrOfStds.push('Ayodele')
 
 let otherstudent = ['jonah', 'dami']
 let otherstudent2 =['bene', 'amin']
-//add to an array
+//insert new elements to an array
 arrOfStds.push(...otherstudent)
 
-//add to a particular index number
+//insert new element at a particular index number / remove an element in the selected index number
 arrOfStds.splice(2,0, 'ayere')
-arrOfStds.splice(3,0,...otherstudent2)
+arrOfStds.splice(3,0, ...otherstudent2)
 arrOfStds.splice(arrOfStds.indexOf('amin')+1,0, 'ayere', 'kehinde')
+arrOfStds.find()
 
 
 console.log(arrOfStds)
